653aac5a580e7f23d081d73066d60ef376efc1c3
Pooja <pooja.shekhara@gmail.com>
2024-11-18 15:20:44 +0530

    Add files via upload

---
+++ Sync.java
@@ -0,0 +1,64 @@
+class Print
+{
+	synchronized void print(String a)
+	{
+		System.out.println("[ " + a + " ]");
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException ie)
+		{
+			System.out.println("Interrupted");
+		}
+	}
+}
+class Thread1 extends Thread
+{
+	Print t;
+	Thread1(Print t)
+	{
+		this.t = t;
+	}
+	public void run()
+	{
+		t.print("Learn");
+	}
+}
+class Thread2 extends Thread
+{
+	Print t;
+	Thread2(Print t)
+	{
+		this.t = t;
+	}
+	public void run()
+	{
+		t.print("Java");
+	}
+}
+class Thread3 extends Thread
+{
+	Print t;
+	Thread3(Print t)
+	{
+		this.t = t;
+	}
+	public void run()
+	{
+		t.print("Programming");
+	}
+}
+class Sync
+{
+	public static void main(String args[])
+	{
+		Print p = new Print();
+		Thread1 t1 = new Thread1(p);
+		Thread2 t2 = new Thread2(p);
+		Thread3 t3 = new Thread3(p);
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
